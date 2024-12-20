005a9e62255d0c3077362f86aa096b6b41a2d3d9
TeeESel <emrandima@gmail.com>
2024-12-20 14:16:24 +0530

    Update First_project.py

--- First_project.py
+++ First_project.py
@@ -1,6 +1,6 @@
 #world of programming
 name= input("Hello! What is your name?")
-print(f"Hello {name}, Welcome to the world of programming!", "I'm TeeESel your companion through this journey" sep="..."
+print(f"Hello {name}, Welcome to the world of programming!", "I'm TeeESel your companion through this journey", sep="...")
 
 
 
